f1e0cfaf4efb94476cad2b920c27ddadd0af7171
ellsa-leung <wmleung74@email.com>
2023-10-04 16:48:33 -0500

    add Scaffold Product Types

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :product_types
   resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
