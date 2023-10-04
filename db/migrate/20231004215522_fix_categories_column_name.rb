e5bac23118fa72cbf404028e2ee6b4b8074decac
ManWahLi <limanwah0924@gmail.com>
2023-10-04 16:56:16 -0500

    fix Categories column name

---
+++ db/migrate/20231004215522_fix_categories_column_name.rb
@@ -0,0 +1,5 @@
+class FixCategoriesColumnName < ActiveRecord::Migration[7.0]
+  def change
+    rename_column :categories, :name, :category_name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_04_205206) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_04_215522) do
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "category_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "product_types", force: :cascade do |t|
+    t.string "product_type_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
