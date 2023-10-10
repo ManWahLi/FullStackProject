84aca855beefd1278b72fb9822df01c25ed332e5
ManWahLi <limanwah0924@gmail.com>
2023-10-10 18:07:04 -0500

    review data sources and import data to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 require "csv"
 require "faker"
+require "net/http"
+require "json"
 
 # Delete all data
 ProductTag.delete_all
@@ -12,36 +14,59 @@ Color.delete_all
 Tag.delete_all
 
 # Reseed id of each table
-ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name IN ('tags', 'brands', 'colors');")
+ActiveRecord::Base.connection.execute(
+  "DELETE FROM sqlite_sequence WHERE name IN (
+    'tags', 'brands', 'colors', 'product_types',
+    'categories', 'products', 'product_colors', 'product_tags');"
+)
 
-# open csv files for Tag
-filename = Rails.root.join("db/tags_list.csv")
-puts "Loading Tag from the CSV file: #{filename}"
+# Import data from a csv file to Color table
+filename = Rails.root.join("db/color.csv")
+puts "Loading Color from the CSV file: #{filename}"
 csv_data = File.read(filename)
-tags = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+colors = CSV.parse(csv_data, headers: true, encoding: "utf-8")
 
-tags.each do |t|
-  Tag.create(tag_name: t["tag_name"].capitalize)
-end
-
-puts "Created #{Tag.count} tags."
-
-# open csv files for Brand
-filename = Rails.root.join("db/brands_list.csv")
-puts "Loading Brand from the CSV file: #{filename}"
-csv_data = File.read(filename)
-brands = CSV.parse(csv_data, headers: true, encoding: "utf-8")
-
-brands.each do |b|
-  Brand.create(brand_name: b["brand_name"].split.map(&:capitalize).join(' '))
-end
-
-puts "Created #{Brand.count} brands."
-
-
-# Import faker data for Color
-20.times do
-  Color.create(color_name: Faker::Color.unique.color_name.capitalize)
+colors.each do |c|
+  Color.create(color_name: c["color_name"])
 end
 
 puts "Created #{Color.count} colors."
+
+# Import data from API and Faker for other tables
+url = "http://makeup-api.herokuapp.com/api/v1/products.json"
+uri = URI(url)
+response = Net::HTTP.get(uri)
+products = JSON.parse(response)
+
+products.each do |p|
+  next unless %w[brand product_type category name price description tag_list image_link
+                 rating].all? do |key|
+                p[key].present?
+              end
+
+  brand = Brand.find_or_create_by(brand_name: p["brand"])
+  category = Category.find_or_create_by(category_name: p["category"])
+  product_type = ProductType.find_or_create_by(product_type_name: p["product_type"])
+  product = brand.products.create(
+    product_name: p["name"],
+    price:        Faker::Commerce.price,
+    description:  Faker::Lorem.sentence,
+    image_link:   p["image_link"],
+    rating:       p["rating"],
+    category:,
+    product_type:
+  )
+
+  # Retrieve data from product tag list and create data in Tag and ProductTag table
+  tags = p["tag_list"]
+  tags.each do |tag_name|
+    tag = Tag.find_or_create_by(tag_name:)
+    ProductTag.create(product:, tag:)
+  end
+
+  # Randomize product colors and create data in ProductColor table
+  rand(1..5).times do
+    color = Color.find(rand(1..20))
+    ProductColor.find_or_create_by(product:, color:)
+  end
+end
