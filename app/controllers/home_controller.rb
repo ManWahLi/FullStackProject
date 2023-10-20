8cc5b81ffb1d97065abd3d3ba1fe1840a176e5cf
ManWahLi <limanwah0924@gmail.com>
2023-10-19 22:50:59 -0500

    add partial for rating star

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @products = Product.includes(:brand)
-    .order("rating DESC")
-    .limit(6)
+    @products = Product.all.where("rating >= 4.5")
+                       .order("RANDOM()")
+                       .limit(6)
   end
 end
