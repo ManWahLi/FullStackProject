af166a97de925de56418442ed359f4654ca38665
ellsa-leung <wmleung74@email.com>
2023-11-21 16:14:22 -0600

    add product_color.csv from web scrapper

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_15_212404) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_18_043827) do
   create_table "brands", force: :cascade do |t|
     t.string "brand_name"
     t.datetime "created_at", null: false
@@ -27,6 +27,51 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_15_212404) do
     t.string "color_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email_address"
+    t.string "address"
+    t.string "city"
+    t.string "postal_code"
+    t.integer "province_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["province_id"], name: "index_customers_on_province_id"
+  end
+
+  create_table "order_products", force: :cascade do |t|
+    t.decimal "order_price"
+    t.decimal "pst"
+    t.decimal "gst"
+    t.integer "order_quantity"
+    t.integer "order_id", null: false
+    t.integer "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_products_on_order_id"
+    t.index ["product_id"], name: "index_order_products_on_product_id"
+  end
+
+  create_table "order_statuses", force: :cascade do |t|
+    t.string "status_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "payment_date"
+    t.datetime "delivery_date"
+    t.integer "customer_id", null: false
+    t.integer "order_status_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+    t.index ["order_status_id"], name: "index_orders_on_order_status_id"
   end
 
   create_table "product_colors", force: :cascade do |t|
@@ -69,12 +114,25 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_15_212404) do
     t.index ["product_type_id"], name: "index_products_on_product_type_id"
   end
 
+  create_table "provinces", force: :cascade do |t|
+    t.string "province_code"
+    t.string "province_name"
+    t.decimal "province_tax_rate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "tag_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "customers", "provinces"
+  add_foreign_key "order_products", "orders"
+  add_foreign_key "order_products", "products"
+  add_foreign_key "orders", "customers"
+  add_foreign_key "orders", "order_statuses"
   add_foreign_key "product_colors", "colors"
   add_foreign_key "product_colors", "products"
   add_foreign_key "product_tags", "products"
