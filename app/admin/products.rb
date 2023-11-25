4020cea53b2229f78d8e60a2d1862af0d7891a5c
ManWahLi <limanwah0924@gmail.com>
2023-11-24 22:38:16 -0600

    installed ActiveStorage

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,11 +1,11 @@
 ActiveAdmin.register Product do
-
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :name, :description, :price, :image_link, :rating, :category_id, :brand_id, :product_type_id
+  permit_params :name, :description, :price, :image_link, :rating, :category_id, :brand_id,
+                :product_type_id, :image
   #
   # or
   #
@@ -15,4 +15,18 @@ ActiveAdmin.register Product do
   #   permitted
   # end
 
+  ## Edit object form
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as:   :file,
+                      hint: if f.object.image.present?
+                              image_tag(f.object.image)
+                            else
+                              ""
+                            end
+    end
+    f.actions
+  end
 end
