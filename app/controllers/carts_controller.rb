de69c321c7098a2adb628a1f7134548142cf7fca
ManWahLi <limanwah0924@gmail.com>
2023-11-29 11:14:32 -0600

    Added remove button in cart and add button in product page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,7 +9,11 @@ class CartsController < ApplicationController
     redirect_to root_path
   end
 
+  # DELETE /CART
   def destroy
     # removes params[:id] from cart
+    id = params[:id].to_i
+    session[:shopping_cart].delete(id)
+    redirect_to carts_path
   end
 end
