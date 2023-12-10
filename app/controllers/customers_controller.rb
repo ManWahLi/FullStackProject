71c7e2b027ce61bfb87b5bc7de9df832d36a88eb
ellsa-leung <wmleung74@email.com>
2023-12-09 22:47:30 -0600

    created Login and Logout function for customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,15 @@
 class CustomersController < ApplicationController
+  before_action :load_provinces
+
   def index
   end
 
   def update
+  end
+
+  private
+
+  def load_provinces
+    @provinces = Province.all
   end
 end
